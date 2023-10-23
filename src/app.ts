8063b2c9ac9f27046a0f53ee924df289a36a2101
JhoanLuna <johanluna20008@outlook.com>
2023-10-23 12:01:42 -0500

    Creacion indice de rutas dinamicas

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,11 @@ import "dotenv/config" //ayuda a gestionar las variables de entorno
 import express from 'express';
 import cors from 'cors'; // Permite a los endPoints consumirse de diferentes orgigenes
 
+import { router } from "./routes/index";
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
+app.use(router);
 
 app.listen(PORT, () => console.log(`Listen on PORT: ${PORT}`));
